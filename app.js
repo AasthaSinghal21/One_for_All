36f8ac49311d36ef3c34ae6de3e4754a6e3a97f5
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2023-01-05 04:53:34 -0800

    backend

---
+++ app.js
@@ -0,0 +1,50 @@
+const express=require("express");
+const app=express();
+const ejs=require("ejs");
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.get("/",function(req,res)
+{
+    res.render("index");
+});
+app.get("/login",function(req,res)
+{
+    res.render("login");
+});
+app.get("/signup",function(req,res)
+{
+    res.render("signup");
+});
+app.get("/test",function(req,res)
+{
+    res.render("test-centre");
+});
+app.get("/techer-attendance",function(req,res)
+{
+    res.render("teacher-attendance");
+});
+app.get("/student-attendance",function(req,res)
+{
+    res.render("student-attendance");
+})
+app.get("/student-resources",function(req,res)
+{
+    res.render("student-resource");
+})
+app.get("/student-assignment",function(req,res)
+{
+    res.render("student-assignment");
+})
+app.get("/pricing",function(req,res)
+{
+    res.render("pricing");
+});
+app.post("/login",function(req,res)
+{
+    res.redirect("/student-assignment");
+})
+
+app.listen(3000,function()
+{
+    console.log("3000");
+});
